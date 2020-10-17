e79b583bdc015514b194083321be4c621df00a70
Sougata Halder <55784019+lordtutapp@users.noreply.github.com>
2020-10-17 13:58:32 +0530

    Update game.component.ts

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,112 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+user: boolean = true;
+  click: boolean = false;
+  arr1 : number[]= [0,0,0,0,0,0,0,0,0];
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  btnClick1(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+    (event.target as HTMLButtonElement).textContent = "O";
+    }
+    else{
+      (event.target as HTMLButtonElement).textContent = "X";
+    };
+    this.arr1[0]=1;
+    this.user=!this.user;
+  };
+  btnClick2(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+      (event.target as HTMLButtonElement).textContent = "O";
+      }
+      else{
+        (event.target as HTMLButtonElement).textContent = "X";
+      };
+      this.arr1[1]=1;
+      this.user=!this.user;
+  };
+  btnClick3(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+      (event.target as HTMLButtonElement).textContent = "O";
+      }
+      else{
+        (event.target as HTMLButtonElement).textContent = "X";
+      };
+      this.arr1[2]=1;
+      this.user=!this.user;
+  };
+  
+  btnClick4(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+      (event.target as HTMLButtonElement).textContent = "O";
+      }
+      else{
+        (event.target as HTMLButtonElement).textContent = "X";
+      };
+      this.arr1[3]=1;
+      this.user=!this.user;
+  };
+  btnClick5(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+      (event.target as HTMLButtonElement).textContent = "O";
+      }
+      else{
+        (event.target as HTMLButtonElement).textContent = "X";
+      };
+      this.arr1[4]=1;
+      this.user=!this.user;
+  };
+  btnClick6(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+      (event.target as HTMLButtonElement).textContent = "O";
+      }
+      else{
+        (event.target as HTMLButtonElement).textContent = "X";
+      };
+      this.arr1[5]=1;
+      this.user=!this.user;
+  };
+  btnClick7(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+      (event.target as HTMLButtonElement).textContent = "O";
+      }
+      else{
+        (event.target as HTMLButtonElement).textContent = "X";
+      };
+      this.arr1[6]=1;
+      this.user=!this.user;
+  };
+  btnClick8(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+      (event.target as HTMLButtonElement).textContent = "O";
+      }
+      else{
+        (event.target as HTMLButtonElement).textContent = "X";
+      };
+      this.arr1[7]=1;
+      this.user=!this.user;
+  };
+  btnClick9(event: MouseEvent) {
+    (event.target as HTMLButtonElement).disabled = true;
+    if(this.user){
+      (event.target as HTMLButtonElement).textContent = "O";
+      }
+      else{
+        (event.target as HTMLButtonElement).textContent = "X";
+      };
+      this.arr1[8]=1;
+      this.user=!this.user;
+  };
 }
