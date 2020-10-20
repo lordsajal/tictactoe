017cf08d03078b0f5266bd4e721a38ceb755f87d
lordsajal <das.sajal273@gmail.com>
2020-10-20 10:42:25 +0530

    backend

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -9,8 +11,9 @@ export class SignInComponent implements OnInit {
 
   registrationForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
-
+  constructor(private fb: FormBuilder, public router:Router,public authService:AuthService) {}
+  message : string="";
+  userError:any;
   ngOnInit() {
     this.initRegForm();
   }
@@ -18,8 +21,7 @@ export class SignInComponent implements OnInit {
   initRegForm() {
     this.registrationForm = this.fb.group({
       email: ["", [Validators.required, Validators.email]],
-      password: ["", Validators.required],
-      confirmpassword: ["", Validators.required]
+      password: ["", Validators.required]
     });
   }
 
@@ -31,4 +33,15 @@ toggleFieldTextType() {
   this.fieldTextType = !this.fieldTextType;
 
 }
+
+onRegister(registrationForm){
+  this.authService.login(registrationForm.value.email,registrationForm.value.password).then((data)=>{
+     console.log(data);
+     this.message="you have been logged in successfully";
+     this.router.navigate(['/profile']);
+   }).catch((error)=>{
+  this.userError=error;
+  console.log(error);
+   })
+ }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,89 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
-
+  registrationForm: FormGroup;
+  constructor(private fb: FormBuilder,public router:Router, public authService:AuthService) { }
+message : string="";
+userError:any;
   ngOnInit(): void {
+    this.initRegForm();
   }
 
+  initRegForm() {
+    this.registrationForm = this.fb.group({
+      fName: ['',[Validators.required]],
+      lName: ['',[Validators.required]],
+      email: ['',[Validators.required,Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['',[Validators.required]]
+    },{
+      validator: this.checkIfMatchingPasswords("password","confirmPassword")
+     });
+  }
+
+  fieldTextTypeP: boolean;
+  fieldTextTypeCP: boolean;
+  checkIfMatchingPasswords(passwordKey: string, confirmPasswordKey: string){
+    return (group: FormGroup) => {
+     let password = group.controls[passwordKey];
+     let confirmPassword = group.controls[confirmPasswordKey];
+     if(password.value==confirmPassword.value){
+       return;
+     }
+     else{
+       confirmPassword.setErrors({
+         notEqualToPassword: true
+       })
+     }
+    }
+  }
+
+
+toggleFieldTextTypeP() {
+  this.fieldTextTypeP = !this.fieldTextTypeP;
+}
+toggleFieldTextTypeCP() {
+  this.fieldTextTypeCP = !this.fieldTextTypeCP;
+}
+onRegister(registrationForm){
+console.log(registrationForm.value);
+let email: string = registrationForm.value.email;
+let password: string = registrationForm.value.password;
+let firstName: string = registrationForm.value.fName;
+let lastName: string = registrationForm.value.lName;
+
+
+this.authService.signup(email, password, firstName, lastName).then((user: any) => {
+
+  firebase.firestore().collection("users").doc(user.uid).set({
+    firstName: registrationForm.value.fName,
+    lastName: registrationForm.value.lName,
+    email: registrationForm.value.email,
+    photoURL: user.photoURL,
+
+  }).then(() => {
+    this.message = "You have been signed up successfully.";
+    this.userError = null;
+    this.router.navigate(['/profile']);
+  })
+
+
+}).catch((error) => {
+  console.log(error);
+  this.userError = error;
+})
+
+
+
+
+}
 }
