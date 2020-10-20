4fa7d48d0c0a7ac4e34a2bb752b4286d33e928c9
swajal das <42321549+lordsajal@users.noreply.github.com>
2020-10-20 19:42:40 +0530

    Update sign-in.component.ts
    fb link

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -9,8 +11,9 @@ export class SignInComponent implements OnInit {
 
   registrationForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
-
+  constructor(private fb: FormBuilder, public router:Router,public authService:AuthService) {}
+  message : string="";
+  userError:any;
   ngOnInit() {
     this.initRegForm();
   }
@@ -30,4 +33,15 @@ toggleFieldTextType() {
   this.fieldTextType = !this.fieldTextType;
 
 }
+
+onRegister(registrationForm){
+  this.authService.login(registrationForm.value.email,registrationForm.value.password).then((data)=>{
+     console.log(data);
+     this.message="you have been logged in successfully";
+     this.router.navigate(['/profile']);
+   }).catch((error)=>{
+  this.userError=error;
+  console.log(error);
+   })
+ }
 }
