45c0584d3131207582079567c2d55b7baba07e23
swajal das <42321549+lordsajal@users.noreply.github.com>
2020-10-20 19:28:46 +0530

    Add files via upload
    firebase link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
+import { ProfileComponent } from './profile/profile.component';
+import { GameComponent } from './game/game.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'', redirectTo:'signIn',pathMatch:'full'
+},{
+  path:'signIn',component:SignInComponent
+},{
+  path:'signUp',component:SignUpComponent
+},{
+  path:'profile',component:ProfileComponent
+},{
+  path:'game',component:GameComponent
+},{
+  path:'**', redirectTo:'signIn'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
